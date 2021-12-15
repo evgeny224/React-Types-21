7823af97fd19c66f3073e778abf00ab2e97fe25f
Evgeny <80065479+evgeny224@users.noreply.github.com>
2021-12-15 11:14:22 +0300

    add ProductCardLIst

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import ProductCard from "./ProductCard";
-import {
-  CasioClock,
-  InvictaClock,
-  CitizenClock,
-  SeikoClock,
-} from "./Data/clockData";
+import ProductCardList from "./ProductCardList";
+import * as productData from "./Data/clockData";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="product__card">
       <Header />
-      <div className="product__card-list">
-        <ProductCard product={CasioClock} />
-        <ProductCard product={InvictaClock} />
-        <ProductCard product={CitizenClock} />
-        <ProductCard product={SeikoClock} />
+      <div>
+        <ProductCardList product={productData} />
       </div>
       <Footer />
     </div>
